e83ffb9ce6b9ac14b0694a24d39d95ff2071ff85
keeper83tarasov <keeper83tarasov@yandex.ru>
2023-03-01 19:19:20 +0300

    master

---
+++ src/main/java/com/tarasov/kteliquibase/KteLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.tarasov.kteliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KteLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KteLiquibaseApplication.class, args);
+	}
+
+}
